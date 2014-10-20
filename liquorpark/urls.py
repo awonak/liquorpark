4698b11ebcb5f9ff8a5736b13c494557475245b7
Adam Wonak <adam@getfoodgenius.com>
2014-10-19 20:03:54 -0500

    Updated more text, added contact functionality, added some images

--- liquorpark/urls.py
+++ liquorpark/urls.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'promo.views.index', name='index'),
+    url(r'^about$', 'promo.views.about', name='index'),
+    url(r'^proprietors$', 'promo.views.proprietors', name='proprietors'),
+    url(r'^contact$', 'promo.views.contact', name='contact'),
+    url(r'^contact/thanks$', 'promo.views.contact_thanks', name='contact-thanks'),
+
+
     #url(r'^admin/', include(admin.site.urls)),
 )
