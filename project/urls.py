4263e1a4c27c86bfb44cdc8436833d01d19fcec7
Adam Wonak <adam.wonak@gmail.com>
2015-12-13 14:02:54 -0600

    new lets encrypt challenge

--- project/urls.py
+++ project/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^robots.txt', TemplateView.as_view(template_name='robots.txt',
         content_type='text/plain')),
 
-    url(r'^.well-known/acme-challenge/frzk-hVve43zKw0NqAEEKFykVUlpbXUaPu_j05FLn8o', TemplateView.as_view(template_name='letsencrypt.txt', content_type='text/plain')),
+    url(r'^.well-known/acme-challenge/sBDdZwJcfNXlYH5qW66Z6kcHNsgDHEqcMVk0cMOYavI', TemplateView.as_view(template_name='letsencrypt.txt', content_type='text/plain')),
 
     # admin
     url(r'^a/', include(admin.site.urls)),
