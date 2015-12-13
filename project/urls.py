6cc3815c72327f459b2c86a08704e2e426dd932c
Adam Wonak <adam.wonak@gmail.com>
2015-12-12 18:24:52 -0600

    serve letsencrypt challenge

--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     url(r'^robots.txt', TemplateView.as_view(template_name='robots.txt',
         content_type='text/plain')),
 
+    url(r'^.well-known/acme-challenge/frzk-hVve43zKw0NqAEEKFykVUlpbXUaPu_j05FLn8o', TemplateView.as_view(template_name='letsencrypt.txt', content_type='text/plain')),
+
     # admin
     url(r'^a/', include(admin.site.urls)),
 ]
